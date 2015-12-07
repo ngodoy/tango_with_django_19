82a1d19afd42f939c8f8a3b23a26e10f6dfe7f7b
godoy <godoy>
2015-12-06 21:43:14 -0430

    add files

--- tango_with_django_project_19/settings.py
+++ tango_with_django_project_19/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'polls.apps.PollsConfig',
-    #'rango.apps.RangoConfig',
+    'polls.apps.PollsConfig',
+    'rango.apps.RangoConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
