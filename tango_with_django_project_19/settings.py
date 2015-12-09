a73dc375fdaca8f0e3c0c612220c9d480c14c0dc
godoy <godoy>
2015-12-09 05:14:33 -0430

    add 404

--- tango_with_django_project_19/settings.py
+++ tango_with_django_project_19/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '1rs-wn+u2(j($7&k$x7t6fsk@9uoc+45(!abu%lp7%#eoxrfbz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
